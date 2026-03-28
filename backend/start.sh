9c229d2e3fb0f8414d647621d459d24387db0eae
Jhonathan Barrios Camargo <jbarrioscamargo@gmail.com>
2026-03-28 13:59:40 -0500

    ajuste de seed

--- backend/start.sh
+++ backend/start.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "🔧 Running Prisma migrations..."
-npx prisma migrate deploy
+npx prisma migrate deploy --verbose
 
 echo "🌱 Running seed..."
-npx prisma db seed
+npx prisma db seed --verbose
 
 echo "🚀 Starting NestJS..."
 node dist/main
